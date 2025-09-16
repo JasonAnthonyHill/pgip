3da864d8101ae0677d32739e88285d5427e4cac0
Jason Hill <hillgiantj@gmail.com>
2025-09-16 19:36:00 +0200

    Improve scan and install
    Updated the scan with some edits for clarity. Added install script to make required packages accessible.

---
+++ install_required_packages.R
@@ -0,0 +1,6 @@
+# Install required packages
+install.packages("rlang")
+install.packages("tidyverse")
+install.packages("data.table")
+install.packages("knitr")
+install.packages("vcfR", dependencies = TRUE)
